eefd64a926b407efa6a02080c2bbfa2b18b483de
powerbox1000 <72870790+powerbox1000@users.noreply.github.com>
2023-06-12 13:47:39 -0400

    Hopefully fix shader loading bug

--- client/shaders.js
+++ client/shaders.js
@@ -1,11 +1,7 @@
 // Yay shaders
-if(typeof window.loadShader !== "function"){
-  throw new Error("window.loadShader is missing")
-}
-
 function loadColor(name, color){
   return loadShader(name, null, `
-    vec4 frag(vec2 pos, vec2 uv, vec4 color, sampler2D tex) {
+    vec4 frag(vec3 pos, vec2 uv, vec4 color, sampler2D tex) {
       vec4 c = def_frag();
       return mix(c, vec4(${(color[0] / 255).toFixed(2)} - c.r, ${(color[1] / 255).toFixed(2)} - c.g, ${(color[2] / 255).toFixed(2)} - c.b, c.a), 2.0);
     }
